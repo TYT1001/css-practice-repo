a5dc803dd7728003cdb4a16d3424e7021ed45e18
TYT22 <tyt22yearsold@gmail.com>
2022-02-13 10:59:23 +0630

    1,2 upload

---
+++ 2-flex-items/index.js
@@ -0,0 +1,15 @@
+function listener(event) {
+  var l = document.createElement("li");
+  switch (event.type) {
+    case "animationstart":
+      l.textContent = `Started: elapsed time is ${event.elapsedTime}`;
+      break;
+    case "animationend":
+      l.textContent = `Ended: elapsed time is ${event.elapsedTime}`;
+      break;
+    case "animationiteration":
+      l.textContent = `New loop started at time ${event.elapsedTime}`;
+      break;
+  }
+  document.getElementById("output").appendChild(l);
+}
